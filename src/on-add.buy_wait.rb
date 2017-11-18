23c3751f8cdb8c7ac64bcf584aa517c39f66d2c2
German Lashevich <german.lashevich@gmail.com>
2017-11-18 01:15:45 +0100

    Typo in report message

--- src/on-add.buy_wait.rb
+++ src/on-add.buy_wait.rb
@@ -14,7 +14,7 @@ new = JSON.parse new, {:symbolize_names => true}
 if /^Buy$|^Buy\./ =~ new[:project] and not new.key?(:due) and not old.key?(:wait)
     new[:wait] = 'someday'
     puts new.to_json
-    puts 'Set due:someday for Buy* project'
+    puts 'Set wait:someday for Buy* project'
 else
     puts new.to_json
 end
