9af0a97843e5873b54f74a5a8861dd69174bb43f
German Lashevich <german.lashevich@gmail.com>
2019-02-12 22:23:05 +0100

    Print some feedback from commit hook

--- src/on-exit.commit.py
+++ src/on-exit.commit.py
@@ -4,7 +4,11 @@ import os
 import subprocess
 import sys
 
-TASK_DIR = os.path.join(os.path.dirname(__file__), os.pardir)
+TASK_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+if subprocess.call("git rev-parse --quiet".split()) != 0:
+    print(f"{TASK_DIR} is not a git repository, skip commiting changes")
+    sys.exit(1)
 
 c = {}
 if len(sys.argv) > 1:
@@ -18,3 +22,14 @@ os.chdir(TASK_DIR)
 if subprocess.call("git diff --exit-code --quiet".split()) != 0:
     subprocess.call("git commit -a".split() + ["-m" + c["args"]])
 
+
+def r(cmd: str) -> str:
+    return subprocess.run(cmd.split(), capture_output=True).stdout.decode().strip()
+
+
+local = r(r"git rev-parse @")
+remote = r(r"git rev-parse @{u}")
+base = r(r"git merge-base @ @{u}")
+
+if local != remote and remote == base:
+    print(f"There are not published changes. Run 'cd {TASK_DIR}; git push'")
