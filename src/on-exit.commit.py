69f039798037c1a2f4fc17342f4ddf3919855e81
German Lashevich <german.lashevich@gmail.com>
2019-02-12 19:22:24 +0100

    Refactoring

--- src/on-exit.commit.py
+++ src/on-exit.commit.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import subprocess
 import sys
 
-TASK_DIR = os.path.dirname(__file__) + os.sep + os.pardir
+TASK_DIR = os.path.join(os.path.dirname(__file__), os.pardir)
 
 c = {}
 if len(sys.argv) > 1:
@@ -15,12 +15,12 @@ if "args" not in c:
 
 os.chdir(TASK_DIR)
 
-if not os.path.isdir(TASK_DIR + os.sep + ".git"):
+if not os.path.isdir(os.path.join(TASK_DIR, ".git")):
     subprocess.call("git init".split())
     with open(".gitignore", "w") as f:
         f.write("*\n")
     subprocess.call("git add -f .gitignore pending.data completed.data".split())
-    subprocess.call(["git", "commit", "-mInitial log"])
+    subprocess.call("git commit -m Initial".split())
 
 if subprocess.call("git diff --exit-code --quiet".split()) != 0:
     subprocess.call("git commit -a".split() + ["-m" + c["args"]])
